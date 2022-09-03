aa7022d504b2a75e8f5535ed56f028d433006fc9
Dhrumil Shah <dhrumilshah@Dhrumils-Air.lan>
2022-09-03 16:02:30 -0400

    Sym file added

--- code/sym.py
+++ code/sym.py
@@ -1,1 +1,25 @@
-print("test commit from sym branch")
+import math
+
+
+class Sym:
+    def Sym_add(self):
+        C.append((input("Enter a char"))[0])
+
+    def calculate_mode(self):
+        print("Mode of List C is % s" % (max(set(C), key=C.count)))
+
+    def calculate_entropy(self):
+        en = 0
+        for i in P:
+            en = en + i*(math.log2(i))
+        en = en*(-1)
+        print("Entropy is", en)
+
+
+C = ['@', '#', '#', '%', '*']
+P = [0.1, 2, 5, 0.23, 3]
+
+s1 = Sym()
+s1.Sym_add()
+s1.calculate_mode()
+s1.calculate_entropy()
