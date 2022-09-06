da94cd04b980cdd62c5f8e2993f8b368b8c6776e
Yashasya <yashasya.shah@gmail.com>
2022-09-05 20:57:53 -0400

    Linked main with other Files

--- code/main.py
+++ code/main.py
@@ -1,5 +1,19 @@
 import cli
+import config
+import num
+import random
 
 obj = cli.CLI()
 the = obj.cli(obj.the)
+config.settings["nums"] = obj.the['nums']
 print(obj.the)
+
+def populate_num():
+    n = num.Num()
+    for i in range(1, 1001):
+        n.add(random.randint(1,i+1))
+    print(n.nums())
+    print(len(n._has))
+
+
+populate_num()
