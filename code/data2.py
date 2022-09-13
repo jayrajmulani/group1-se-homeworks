230b7e61c21a7a38466e489839bc3c48561652e4
Harshil Sanghavi <harshilsan7@gmail.com>
2022-09-12 22:46:44 -0400

    Added function Add and Stats in data2

--- code/data2.py
+++ code/data2.py
@@ -1,6 +1,6 @@
 
-from utils import csv
-
+from utils import *
+from cols import *
 
 class data:
     def __init__(self, src=None) -> None:
@@ -14,7 +14,23 @@ class data:
                 self.add(row)
 
 
-    def add(self,xs, row=None):
-        print("row")
+    def add(self, xs , row= None):
+        if not self.cols :
+            self.cols = Cols(xs)
+        else :
+            row.append(self.row, xs.cells and xs or Row(xs))
+            for todo in [self.cols.x, self.cols.y]:
+                for col in[todo]:
+                    col.add(self,row.cells[col.at])
+                
+                
+    def stats(self,showCols,fun,v, places=None,t= None):
+        showCols, fun = showCols or self.cols.y, fun or "mid"
+        t={}
+        for col in [showCols]:
+            v=fun(col)
+            v=type(v)=="number" and rnd(v,places) or v
+            t[col.name]=v 
+        return t
 
             
